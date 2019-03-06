5732d7efdff7dbdf1ed7bffd31113d379ee963f2
Andrew Stout <stoutar@miamioh.edu>
2019-03-06 15:57:23 -0500

    Long overdue update!

---
+++ datamodel.php
@@ -0,0 +1,229 @@
+<?php
+require_once('config.php');
+
+//1. Fetch Property Given Id
+//2. Insert New Property
+//3. Fetch Reviews given PropertyId
+//4. Insert Review
+
+// connect to the database
+function connectDB(){
+    $mysqli = mysqli_connect(MYSQL_HOST, MYSQL_USER,MYSQL_PW,MYSQL_DB, MYSQL_PORT);
+    if (mysqli_connect_errno($mysqli)) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+				die;
+	}else{
+        return $mysqli;
+    }
+}
+
+function selectAllProperties()
+{
+	$mysqli = connectDB();
+	$stmt = $mysqli->prepare("SELECT * FROM property;");
+	if(!$stmt->execute()) {
+		//echo "error executing query" . $stmt->error;
+		return array('status'=>'FAIL', 'msg'=>"error executing query" . $stmt->error);
+	} else {
+		$stmt->bind_result($pk, $address, $description,$beds, $baths, $manager, $city, $state, $zip, $propertyType, $name, $occupancy, $aptNumber);
+		$propertyList = array();
+		while($stmt->fetch()) {
+			$property = array('id'=>$pk, 'address'=>$address, 'beds'=>$beds, 'baths'=>$baths, 'manager'=>$manager, 'city'=>$city, 'state'=>$state, 
+						'zip'=>$zip, 'propertyType'=>$propertyType, 'name'=>$name, 'occupancy'=>$occupancy, 'aptNumber'=>$aptNumber);
+			array_push($propertyList, $property);
+			
+			//Can use for debugging. Remove before release.
+			//echo $pk . ", " . $address . ", " . $description;
+		}
+		
+		//Extra check to protect against unintended side effects
+		if (count($propertyList) == 0) {
+			return array('status'=>'FAIL', 'msg'=>"No Properties Found");
+		} else {
+			return array('status'=>'OK', 'msg'=>'Success', 'properties'=>$propertyList);
+		}
+	}
+}
+
+// Fetch Property Given Id
+function findPropertyById($id){
+    $conn = connectDB();
+    $sql = "SELECT * FROM property WHERE pk=?";
+ 
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+
+    if(!$stmt->execute()) {
+        echo "error executing query" . $stmt->error;
+        $arr = array('status'=>'FAIL', 'msg'=>"error executing query" . $stmt->error);
+    }else{
+        $stmt->bind_result($pk, $address, $description, $beds, $baths, $managerName, 
+        $city, $state, $zipCode, $propertyType, $name, $occupancy, $apt_number);
+        $stmt->fetch();
+        if(isset($pk)) {
+            $status = 'OK';
+            $msg = 'SUCCESS';
+            $arr = array('status'=>$status, 'msg'=>$msg, 'address'=>$address, 
+            'description'=>$description,'beds'=>$beds,'baths'=>$baths,'managerName'=>$managerName,
+            'city'=>$city,'state'=>$state,'zipCode'=>$zipCode,'propertyType'=>$propertyType,
+            'name'=>$name,'occupancy'=>$occupancy,'apt_number'=>$apt_number);
+        } else {
+            $status = 'FAIL';
+            $msg = 'PROPERTY NOT FOUND';
+            $arr = array('status'=>$status, 'msg'=>$msg);
+        }
+    }
+    $stmt->close();
+    $conn->close();
+    return $arr;
+}
+
+// Fetch Reviews given PropertyId
+function findReviewsById($propertyID){
+    $conn = connectDB();
+    $sql = "SELECT * FROM review WHERE propertyID=?";
+ 
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $propertyID);
+
+    if(!$stmt->execute()) {
+        echo "error executing query" . $stmt->error;
+        $arr = array('status'=>'FAIL', 'msg'=>"error executing query" . $stmt->error);
+    }else{
+        $stmt->bind_result($pk, $recommended, $rent, $maintenance, $neighborhood, $body, 
+        $propertyID);
+		$reviewList = array();
+        while($stmt->fetch()) {
+			$review = array('id'=>$pk, 'recommended'=>$recommended, 'rent'=>$rent, 'maintenance'=>$maintenance, 
+				'neighborhood'=>$neighborhood, 'body'=>$body, 'propertyID'=>$propertyID);
+			array_push($reviewList, $review);
+		}
+		
+        if(count($reviewList) > 0) {
+            $status = 'OK';
+            $msg = 'SUCCESS';
+            $arr = array('status'=>$status, 'msg'=>$msg, 'reviews'=>$reviewList);
+        } else {
+            $status = 'FAIL';
+            $msg = 'REVIEWS NOT FOUND';
+            $arr = array('status'=>$status, 'msg'=>$msg);
+        }
+    }
+    $stmt->close();
+    $conn->close();
+    return $arr;
+}
+
+// Insert New Property
+function addProperty($address, $description, $beds, $baths, $managerName, 
+$city, $state, $zipCode, $propertyType, $name, $occupancy, $apt_number){
+    $conn = connectDB();
+    $sql = "INSERT INTO property (address, description, beds, baths, managerName,
+    city,state,zipCode,propertyType,name,occupancy,apt_number) 
+    VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssiissssssii", $address, $description, $beds, $baths,
+    $managerName, $city, $state, $zipCode, $propertyType, $name, $occupancy, $apt_number);
+
+    if(!$stmt->execute()) {
+        echo "FAILED TO INSERT PROPERTIES" . $stmt->error;
+        $arr = array('status'=>'FAIL', 'msg'=>"FAILED TO INSERT PROPERTIES" . $stmt->error);
+    }else{
+        $status = 'OK';
+		$msg = 'SUCCESS';
+		$arr = array('status'=>$status, 'msg'=>$msg);
+    }
+    $stmt->close();
+    $conn->close();
+    return $arr;
+}
+
+// Insert Review
+function addReview($recommended, $rent, $maintenance, $neighborhood, $body, $propertyID){
+    $conn = connectDB();
+    $sql = "INSERT INTO review (recommended, rent, maintenance, neighborhood, body, propertyID) 
+    VALUES (?,?,?,?,?,?)";
+	$temp = (int) $propertyID;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("iisssi", $recommended, $rent, $maintenance, $neighborhood, $body, $temp);
+	$arr = array();
+    if(!$stmt->execute()) {
+        echo "FAILED TO INSERT REVIEW" . $stmt->error;
+        $arr = array('status'=>'FAIL', 'msg'=>"FAILED TO INSERT REVIEW" . $stmt->error);
+    }else{
+        $status = 'OK';
+		$msg = 'SUCCESS';
+		$arr = array('status'=>$status, 'msg'=>$msg);
+    }
+    $stmt->close();
+    $conn->close();
+    return $arr;
+}
+
+// Search by properties name or address
+function keywordSearch($keyword){
+$conn = connectDB();
+    $sql = "SELECT * FROM property WHERE name LIKE ? OR address LIKE ?";
+ 
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $keyword);
+
+    if(!$stmt->execute()) {
+        echo "error executing query" . $stmt->error;
+        $arr = array('status'=>'FAIL', 'msg'=>"error executing query" . $stmt->error);
+    }else{
+        $stmt->bind_result($pk, $address, $description, $beds, $baths, $managerName, 
+        $city, $state, $zipCode, $propertyType, $name, $occupancy, $apt_number);
+        $stmt->fetch();
+        if(isset($pk)) {
+            $status = 'OK';
+            $msg = 'SUCCESS';
+            $arr = array('status'=>$status, 'msg'=>$msg, 'address'=>$address, 
+            'description'=>$description,'beds'=>$beds,'baths'=>$baths,'managerName'=>$managerName,
+            'city'=>$city,'state'=>$state,'zipCode'=>$zipCode,'propertyType'=>$propertyType,
+            'name'=>$name,'occupancy'=>$occupancy,'apt_number'=>$apt_number);
+        } else {
+            $status = 'FAIL';
+            $msg = 'PROPERTY NOT FOUND';
+            $arr = array('status'=>$status, 'msg'=>$msg);
+        }
+    }
+    $stmt->close();
+    $conn->close();
+    return $arr;
+}
+
+function selectPropertiesByCategory($type)
+{
+	$conn = connectDB();
+	$sql = "SELECT * FROM property WHERE propertyType=?;";
+	
+	$stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $type);
+	if(!$stmt->execute()) {
+		//echo "error executing query" . $stmt->error;
+		return array('status'=>'FAIL', 'msg'=>"error executing query" . $stmt->error);
+	} else {
+		$stmt->bind_result($pk, $address, $description,$beds, $baths, $manager, $city, $state, $zip, $propertyType, $name, $occupancy, $aptNumber);
+		$propertyList = array();
+		while($stmt->fetch()) {
+			$property = array('id'=>$pk, 'address'=>$address, 'beds'=>$beds, 'baths'=>$baths, 'manager'=>$manager, 'city'=>$city, 'state'=>$state, 
+						'zip'=>$zip, 'propertyType'=>$propertyType, 'name'=>$name, 'occupancy'=>$occupancy, 'aptNumber'=>$aptNumber);
+			array_push($propertyList, $property);
+			
+			//Can use for debugging. Remove before release.
+			//echo $pk . ", " . $address . ", " . $description;
+		}
+		
+		//Extra check to protect against unintended side effects
+		if (count($propertyList) == 0) {
+			return array('status'=>'FAIL', 'msg'=>"No Properties Found");
+		} else {
+			return array('status'=>'OK', 'msg'=>'Success', 'properties'=>$propertyList);
+		}
+	}
+}
+        
+?>
---
+++ restUtilities.php
@@ -0,0 +1,46 @@
+<?php
+/* RestUtilities.php
+    DESCRIPTION: This file contains a utility functions for the Uptown-Index project REST API
+	@author : drews
+	@version 0.2
+*/
+
+//JSON HELPER FUNCTION
+function getJson() 
+{
+	$jsonStringIn = file_get_contents('php://input');
+	$json = array();
+	$response = array();
+	try {
+		$json = json_decode($jsonStringIn,true);
+		return $json;
+	}
+	catch (Exception $e) {
+		
+		header("HTTP/1.0 500 Invalid content -> probably invalid JSON format");
+		$response['status'] = "fail";
+		$response['message'] = $e->getMessage();
+		print json_encode($response);
+		exit;
+	}
+
+}
+
+//ERROR RESPONSE FUNCTION - INVALID PATH
+function invalidPath()
+{
+	//header("HTTP/1.1 404 Not Found");
+	$output['status'] = 'FAIL';
+	$output['msg'] = 'INVALID PATH';
+	
+	return $output;
+}
+
+//ERROR RESPONSE FUNCTION - INVALID METHOD
+function invalidMethod()
+{
+	//header("HTTP/1.1 405 Method Not Allowed");
+	$output['status'] = 'FAIL';
+	$output['msg'] = 'INVALID METHOD';
+}
+
